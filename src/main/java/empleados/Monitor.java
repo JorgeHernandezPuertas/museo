b703f878aec2bae16f603fcd241218437e894dd2
JorgeHernandezPuertas <jherpue553@g.educaand.es>
2023-03-18 13:21:01 +0100

    feat: created final class Monitor

---
+++ src/main/java/empleados/Monitor.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package empleados;
+
+/**
+ *
+ * @author jorge
+ */
+public class Monitor extends Empleado {
+    
+    // Atributo (para que no se quede vacía)
+    private String especialidadTour;
+    
+    // Constructores
+    public Monitor(String especialidadTour) {
+        this.especialidadTour = especialidadTour;
+    }
+
+    public Monitor(String especialidadTour, String nif, String nombre) {
+        super(nif, nombre);
+        this.especialidadTour = especialidadTour;
+    }
+    
+    // Getter y setter
+    public String getEspecialidadTour() {
+        return especialidadTour;
+    }
+
+    public void setEspecialidadTour(String especialidadTour) {
+        this.especialidadTour = especialidadTour;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(super.toString());
+        sb.append("Monitor{");
+        sb.append("especialidadTour=").append(especialidadTour);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+}
