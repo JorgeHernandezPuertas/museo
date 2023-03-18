ae601fcc22219ee103e9d197ad883fd96fdc72cb
JorgeHernandezPuertas <jherpue553@g.educaand.es>
2023-03-18 14:02:37 +0100

    feat: created final class cuadro

---
+++ src/main/java/obras/Cuadro.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package obras;
+
+/**
+ *
+ * @author jorge
+ */
+public final class Cuadro extends Obra {
+    
+    // Atributo
+    private String estilo;
+
+    // Constructor
+    public Cuadro(String estilo, String id, String autor) {
+        super(id, autor);
+        this.estilo = estilo;
+    }
+
+    // Getter y setter
+    public String getEstilo() {
+        return estilo;
+    }
+
+    public void setEstilo(String estilo) {
+        this.estilo = estilo;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(super.toString());
+        sb.append("Cuadro{");
+        sb.append("estilo=").append(estilo);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+}
