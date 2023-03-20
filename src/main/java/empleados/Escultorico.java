e28412e820991ea72309bc6e234666c39f28a52e
JorgeHernandezPuertas <jherpue553@g.educaand.es>
2023-03-20 14:28:58 +0100

    feat: implements restaurador in escultorico

--- src/main/java/empleados/Escultorico.java
+++ src/main/java/empleados/Escultorico.java
@@ -4,10 +4,25 @@
  */
 package empleados;
 
+import obras.Obra;
+import obras.Pintura;
+
 /**
  *
  * @author jorge
  */
 public final class Escultorico extends Conservador {
+
+    @Override
+    public void restaurar(Obra obra) {
+        // Como es escultorico hago que restaure solo las obras que son esculturas
+        if (obra instanceof Pintura){
+            System.out.println("El escultorico " + this.getNombre() + " ha restaurado "
+                    + "la escultura " + obra.getId());
+        } else {
+            System.out.println("El restaurador es escultorico y no puede restaurar"
+                    + " una pintura.");
+        }
+    }
     
 }
