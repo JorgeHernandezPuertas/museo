c46ca18180fa08a8bd05ae666e181c4e89fbb2d6
JorgeHernandezPuertas <jherpue553@g.educaand.es>
2023-03-20 14:10:36 +0100

    feat: created new interface restaurador

---
+++ src/main/java/empleados/Restaurador.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package empleados;
+
+import obras.Obra;
+
+/**
+ *
+ * @author jorge
+ */
+public interface Restaurador {
+    
+    // Método abstracto para restaurar
+    public abstract void restaurar(Obra obra);
+}
